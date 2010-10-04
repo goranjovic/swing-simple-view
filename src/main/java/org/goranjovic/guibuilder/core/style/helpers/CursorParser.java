9e226a92f0d3ea88e01b96cccf7603b04481081f
Goran Jovic <Goran Jovic@localhost>
2010-10-04 21:14:35 +0200

    refactored cursor parser

---
+++ src/main/java/org/goranjovic/guibuilder/core/style/helpers/CursorParser.java
@@ -0,0 +1,54 @@
+package org.goranjovic.guibuilder.core.style.helpers;
+
+import java.awt.Cursor;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.net.URL;
+
+public class CursorParser {
+	
+	public Cursor parseCursor(String cursorName) {
+		Cursor cursor = new Cursor(Cursor.DEFAULT_CURSOR);
+		
+		if(cursorName.equalsIgnoreCase("crosshair")){
+			cursor = new Cursor(Cursor.CROSSHAIR_CURSOR);
+		}else if(cursorName.equalsIgnoreCase("default")){
+			cursor = new Cursor(Cursor.DEFAULT_CURSOR);
+		}else if(cursorName.equalsIgnoreCase("pointer")){
+			cursor = new Cursor(Cursor.HAND_CURSOR);
+		}else if(cursorName.equalsIgnoreCase("move")){
+			cursor = new Cursor(Cursor.MOVE_CURSOR);
+		}else if(cursorName.equalsIgnoreCase("e-resize")){
+			cursor = new Cursor(Cursor.E_RESIZE_CURSOR);
+		}else if(cursorName.equalsIgnoreCase("ne-resize")){
+			cursor = new Cursor(Cursor.NE_RESIZE_CURSOR);
+		}else if(cursorName.equalsIgnoreCase("nw-resize")){
+			cursor = new Cursor(Cursor.NW_RESIZE_CURSOR);
+		}else if(cursorName.equalsIgnoreCase("n-resize")){
+			cursor = new Cursor(Cursor.N_RESIZE_CURSOR);
+		}else if(cursorName.equalsIgnoreCase("se-resize")){
+			cursor = new Cursor(Cursor.SE_RESIZE_CURSOR);
+		}else if(cursorName.equalsIgnoreCase("sw-resize")){
+			cursor = new Cursor(Cursor.SW_RESIZE_CURSOR);
+		}else if(cursorName.equalsIgnoreCase("s-resize")){
+			cursor = new Cursor(Cursor.S_RESIZE_CURSOR);
+		}else if(cursorName.equalsIgnoreCase("w-resize")){
+			cursor = new Cursor(Cursor.W_RESIZE_CURSOR);
+		}else if(cursorName.equalsIgnoreCase("text")){
+			cursor = new Cursor(Cursor.TEXT_CURSOR);
+		}else if(cursorName.equalsIgnoreCase("wait")){
+			cursor = new Cursor(Cursor.WAIT_CURSOR);
+		}else if(cursorName.startsWith("url")){
+			UrlParser parser = new UrlParser();
+			URL url = parser.parseURL(cursorName);
+			Toolkit toolkit = Toolkit.getDefaultToolkit();
+			Image image = toolkit.createImage(url);
+			cursor = toolkit.createCustomCursor(image, new Point(0,0), url.getFile());
+			
+		}
+		return cursor;
+	}
+
+
+}
--- src/main/java/org/goranjovic/guibuilder/core/style/stylists/ClassificationStylist.java
+++ src/main/java/org/goranjovic/guibuilder/core/style/stylists/ClassificationStylist.java
@@ -30,9 +30,12 @@ import javax.swing.ImageIcon;
 
 import org.goranjovic.css.dom.StyleSheet;
 import org.goranjovic.guibuilder.core.ElementDescription;
+import org.goranjovic.guibuilder.core.style.helpers.CursorParser;
 import org.goranjovic.guibuilder.core.style.helpers.UrlParser;
 
 public class ClassificationStylist implements Stylist {
+	
+	private CursorParser cursorParser = new CursorParser();
 
 	@Override
 	public void applyStyle(ElementDescription description, StyleSheet style) {
@@ -51,47 +54,11 @@ public class ClassificationStylist implements Stylist {
 
 	private void applyCursor(Component component, String cursorName) {
 		
-		Cursor cursor = new Cursor(Cursor.DEFAULT_CURSOR);
-		
-		if(cursorName.equalsIgnoreCase("crosshair")){
-			cursor = new Cursor(Cursor.CROSSHAIR_CURSOR);
-		}else if(cursorName.equalsIgnoreCase("default")){
-			cursor = new Cursor(Cursor.DEFAULT_CURSOR);
-		}else if(cursorName.equalsIgnoreCase("pointer")){
-			cursor = new Cursor(Cursor.HAND_CURSOR);
-		}else if(cursorName.equalsIgnoreCase("move")){
-			cursor = new Cursor(Cursor.MOVE_CURSOR);
-		}else if(cursorName.equalsIgnoreCase("e-resize")){
-			cursor = new Cursor(Cursor.E_RESIZE_CURSOR);
-		}else if(cursorName.equalsIgnoreCase("ne-resize")){
-			cursor = new Cursor(Cursor.NE_RESIZE_CURSOR);
-		}else if(cursorName.equalsIgnoreCase("nw-resize")){
-			cursor = new Cursor(Cursor.NW_RESIZE_CURSOR);
-		}else if(cursorName.equalsIgnoreCase("n-resize")){
-			cursor = new Cursor(Cursor.N_RESIZE_CURSOR);
-		}else if(cursorName.equalsIgnoreCase("se-resize")){
-			cursor = new Cursor(Cursor.SE_RESIZE_CURSOR);
-		}else if(cursorName.equalsIgnoreCase("sw-resize")){
-			cursor = new Cursor(Cursor.SW_RESIZE_CURSOR);
-		}else if(cursorName.equalsIgnoreCase("s-resize")){
-			cursor = new Cursor(Cursor.S_RESIZE_CURSOR);
-		}else if(cursorName.equalsIgnoreCase("w-resize")){
-			cursor = new Cursor(Cursor.W_RESIZE_CURSOR);
-		}else if(cursorName.equalsIgnoreCase("text")){
-			cursor = new Cursor(Cursor.TEXT_CURSOR);
-		}else if(cursorName.equalsIgnoreCase("wait")){
-			cursor = new Cursor(Cursor.WAIT_CURSOR);
-		}else if(cursorName.startsWith("url")){
-			UrlParser parser = new UrlParser();
-			URL url = parser.parseURL(cursorName);
-			Toolkit toolkit = Toolkit.getDefaultToolkit();
-			Image image = toolkit.createImage(url);
-			cursor = toolkit.createCustomCursor(image, new Point(0,0), url.getFile());
-			
-		}
+		Cursor cursor = cursorParser.parseCursor(cursorName);
 		
 		component.setCursor(cursor);
 		
 	}
 
+	
 }
