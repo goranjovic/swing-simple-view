117ba1ea3e7561800b0a438af665be4fb90dd665
Goran Jovic <Goran Jovic@localhost>
2010-10-04 23:06:45 +0200

    changed project name to swing-simple-view in all license comment headers

--- src/main/java/org/goranjovic/guibuilder/core/localizer/Localizer.java
+++ src/main/java/org/goranjovic/guibuilder/core/localizer/Localizer.java
@@ -1,20 +1,20 @@
-/*SwingGuiBuilder - a Simple View implementation for Swing
+/*swing-simple-view - a Simple View implementation for Swing
 Copyright (C) 2009. Goran Jovic
 
-This file is part of SwingGuiBuilder.
+This file is part of swing-simple-view.
 
-SwingGuiBuilder is free software: you can redistribute it and/or modify
+swing-simple-view is free software: you can redistribute it and/or modify
 it under the terms of the Lesser GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-SwingGuiBuilder is distributed in the hope that it will be useful,
+swing-simple-view is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU Lesser General Public License for more details.
 
 You should have received a copy of the Lesser GNU General Public License
-along with SwingGuiBuilder.  If not, see <http://www.gnu.org/licenses/>.*/
+along with swing-simple-view.  If not, see <http://www.gnu.org/licenses/>.*/
 
 package org.goranjovic.guibuilder.core.localizer;
 
