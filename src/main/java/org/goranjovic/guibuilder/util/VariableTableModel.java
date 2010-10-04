c1e99caea08b9603128033bf0652cf3a7f8598bb
Goran Jovic <Goran Jovic@localhost>
2010-10-04 23:09:44 +0200

    added license comment header to new classes

--- src/main/java/org/goranjovic/guibuilder/util/VariableTableModel.java
+++ src/main/java/org/goranjovic/guibuilder/util/VariableTableModel.java
@@ -1,3 +1,21 @@
+/*swing-simple-view - a Simple View implementation for Swing
+Copyright (C) 2009. Goran Jovic
+
+This file is part of swing-simple-view.
+
+swing-simple-view is free software: you can redistribute it and/or modify
+it under the terms of the Lesser GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+swing-simple-view is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the Lesser GNU General Public License
+along with swing-simple-view.  If not, see <http://www.gnu.org/licenses/>.*/
+
 package org.goranjovic.guibuilder.util;
 
 import java.util.ArrayList;
