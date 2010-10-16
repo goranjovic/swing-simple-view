a21cef98c9cc39f737d11f88c08d56c0c893df78
Goran Jovic <Goran Jovic@localhost>
2010-10-16 14:42:04 +0200

    checkbox value is bound

--- src/main/java/org/goranjovic/guibuilder/util/components/SCheckBox.java
+++ src/main/java/org/goranjovic/guibuilder/util/components/SCheckBox.java
@@ -1,15 +1,22 @@
 package org.goranjovic.guibuilder.util.components;
 
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.beans.PropertyChangeSupport;
 
 import javax.swing.JCheckBox;
 
-public class SCheckBox extends JCheckBox  implements SComponent  {
+public class SCheckBox extends JCheckBox  implements SComponent, ItemListener  {
 
 	private static final long serialVersionUID = -8870247184874383966L;
 	
 	private PropertyChangeSupport pcs = new PropertyChangeSupport(this);
 
+	public SCheckBox(){
+		super();
+		addItemListener(this);
+	}
+	
 	@Override
 	public void setValue(Object value) {
 		setSelected((Boolean)value);
@@ -30,6 +37,13 @@ public class SCheckBox extends JCheckBox  implements SComponent  {
 	@Override
 	public PropertyChangeSupport retrievePropertyChangeSupport() {
 		return pcs;
+	}
+
+	@Override
+	public void itemStateChanged(ItemEvent event) {
+		Boolean value = (Boolean)getValue();
+		Boolean oldValue = !value;
+		pcs.firePropertyChange("selected", oldValue, value);
 	}
 
 }
